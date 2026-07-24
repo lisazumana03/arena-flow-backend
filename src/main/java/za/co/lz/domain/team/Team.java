488fcf0f2b350162beff3d0bc5bc28e9e71291c2
Lisa Zumana <zumanalisa@gmail.com>
2026-07-24 13:41:52 +0200

    file ingooooooaling

--- src/main/java/za/co/lz/domain/team/Team.java
+++ src/main/java/za/co/lz/domain/team/Team.java
@@ -1,8 +1,6 @@
 package za.co.lz.domain.team;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 import java.io.Serializable;

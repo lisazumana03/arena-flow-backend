df0bf2ca1e405d944b530174813a094da670b2ec
Lisa Zumana <zumanalisa@gmail.com>
2026-07-29 20:30:30 +0200

    file ingooooooaling

--- src/main/java/za/co/lz/controller/team/PlayerController.java
+++ src/main/java/za/co/lz/controller/team/PlayerController.java
@@ -1,8 +1,15 @@
 package za.co.lz.controller.team;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +26,23 @@ public class PlayerController {
     public ResponseEntity<Player> createPlayer(@RequestBody Player player){
         Player createdPlayer = playerService.create(player);
         return ResponseEntity.ok(createdPlayer);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Player>> getAllPlayers() {
+        List<Player> players = playerService.findAll();
+        return ResponseEntity.ok(players);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Player> updatePlayer(@PathVariable UUID id, @RequestBody Player player) {
+        Player updatedPlayer = playerService.update(player, id);
+        return ResponseEntity.ok(updatedPlayer);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deletePlayer(@PathVariable UUID id) {   
+        playerService.delete(id);
+            return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/za/co/lz/controller/team/TeamController.java
+++ src/main/java/za/co/lz/controller/team/TeamController.java
@@ -7,6 +7,8 @@ import za.co.lz.domain.team.Team;
 import za.co.lz.service.team.ITeamService;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/team")
@@ -21,6 +23,28 @@ public class TeamController {
         return ResponseEntity.ok(createdTeam);
     }
 
+    @GetMapping("/all")
+    public ResponseEntity<List<Team>> getAllTeams() {
+        List<Team> teams = teamService.findAll();
+        return ResponseEntity.ok(teams);
+    }
 
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Team> updateTeam(@PathVariable UUID id, @RequestBody Team team) {
+        Team updatedTeam = teamService.update(team, id);
+        return ResponseEntity.ok(updatedTeam);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteTeam(@PathVariable UUID id) {
+        teamService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Team>> getTeamById(@PathVariable UUID id) {
+        Optional<Team> team = teamService.findById(id);
+        return ResponseEntity.ok(team);
+    }
 
 }
